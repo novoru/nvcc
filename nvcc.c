41a811271b43f4a401b4f800874a979398e7c786
Noboru Horita <61climb@gmail.com>
2019-06-03 13:36:21 +0900

    単項演算子+、-を実装

--- nvcc.c
+++ nvcc.c
@@ -127,6 +127,7 @@ void tokenize() {
 Node *term();
 Node *mul();
 Node *expr();
+Node *unary();
 
 Node *term() {
   // 次のトークンが'('なら"(" expr ")"のはず
@@ -149,13 +150,13 @@ Node *term() {
 }
 
 Node *mul() {
-  Node *node = term();
+  Node *node = unary();
 
   for(;;) {
     if(consume('*'))
-      node = new_node('*', node, term());
+      node = new_node('*', node, unary());
     else if(consume('/'))
-      node = new_node('/', node, term());
+      node = new_node('/', node, unary());
     else
       return node;
   }
@@ -172,6 +173,15 @@ Node *expr() {
     else
       return node;
   }
+}
+
+Node *unary() {
+  if(consume('+'))
+    return term();
+  if(consume('-'))
+    return new_node('-', new_node_num(0), term());
+  return term();
+  
 }
 
 void gen(Node *node) {
